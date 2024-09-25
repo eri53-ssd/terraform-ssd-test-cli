c14a4304c1d66b3ca30ff1982b993870551049f7
zhang Wei <136400441+cd10961@users.noreply.github.com>
2024-09-25 18:31:47 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,11 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-
 resource "aws_s3_bucket" "ssd_bucket" {
-  bucket = "zssd-trfm-bucket"
+  bucket = "ssd-trfm-bucket"
 
   versioning {
     enabled = true
 }
 
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET","PUT"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 1000
-}
 }
