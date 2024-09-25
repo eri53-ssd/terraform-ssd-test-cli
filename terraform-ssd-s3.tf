bfd03d36a39d0371d744136a397827524bf80696
eri53p <99152467+eri53p@users.noreply.github.com>
2024-09-26 00:43:29 +0900

    Update terraform-ssd-s3.tf

--- terraform-ssd-s3.tf
+++ terraform-ssd-s3.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket_versioning" "ssd_bucket" {
+resource "aws_s3_bucket" "ssd_bucket" {
   bucket = "trfm-ssd-s3-test-state-000001"
 
-  versioning_configuration {
-    status = "Enabled"
-  }
+  versioning {
+    enabled = true
+}
 
 }
