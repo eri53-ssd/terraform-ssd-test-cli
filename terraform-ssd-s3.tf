71903bb743a989583ccb8912bec45457d2f60929
eri53p <99152467+eri53p@users.noreply.github.com>
2024-10-17 23:13:52 +0900

    Update terraform-ssd-s3.tf

--- terraform-ssd-s3.tf
+++ terraform-ssd-s3.tf
@@ -9,14 +9,14 @@ provider "aws" {
     # enabled = true
  #   enabled = false
 #}
-#resource "aws_s3_bucket" "ssd_bucket_cli" {
-#  bucket = "trfm-ssd-s3-test-state-cli-000002"
-#}
+resource "aws_s3_bucket" "ssd_bucket_cli" {
+  bucket = "trfm-ssd-s3-test-state-cli-000002"
+}
 
-#resource "aws_s3_bucket_versioning" "ssd_bucket_cli_versioning" {
-#  bucket = aws_s3_bucket.ssd_bucket_cli.id
+resource "aws_s3_bucket_versioning" "ssd_bucket_cli_versioning" {
+  bucket = aws_s3_bucket.ssd_bucket_cli.id
 
-#  versioning_configuration {
-#    status = "Enabled"
-#  }
-#}
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
